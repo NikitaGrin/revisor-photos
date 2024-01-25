627569a8e9e3d803cc45d42ff3de3a459d8e5799
Name <name@example.com>
2024-01-25 12:49:29 +0300

    Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Catalog from "./pages/Catalog";
+import Favorites from "./pages/Favorites";
+import ErrorPage from "./pages/ErrorPage";
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="catalog" />} />
+          <Route index path="catalog" element=<Catalog /> />
+          <Route index path="favorites" element=<Favorites /> />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/pages/Favorites.tsx
@@ -0,0 +1,5 @@
+function Favorites() {
+  return <div>fav</div>;
+}
+
+export default Favorites;
