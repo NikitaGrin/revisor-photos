488880f6f15a017dc23ce425f85067390b92d7af
Name <name@example.com>
2024-01-25 13:32:48 +0300

    Added navigation

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import Catalog from "./pages/Catalog";
 import Favorites from "./pages/Favorites";
 import ErrorPage from "./pages/ErrorPage";
 import "./index.css";
+import Layout from "./pages/Layout";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to="catalog" />} />
-          <Route index path="catalog" element=<Catalog /> />
-          <Route index path="favorites" element=<Favorites /> />
-          <Route path="*" element={<ErrorPage />} />
+          <Route element={<Layout />}>
+            <Route index element={<Navigate replace to="catalog" />} />
+            <Route index path="catalog" element=<Catalog /> />
+            <Route index path="favorites" element=<Favorites /> />
+            <Route path="*" element={<ErrorPage />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/Layout.tsx
@@ -0,0 +1,18 @@
+import { NavLink, Outlet } from "react-router-dom";
+import styles from "./Layout.module.css";
+
+function Layout() {
+  return (
+    <div className={styles.layout}>
+      <header className={styles.header}>
+        <nav className={styles.nav}>
+          <NavLink to="/catalog">Catalog</NavLink>
+          <NavLink to="/favorites">Favorites</NavLink>
+        </nav>
+      </header>
+      <Outlet />
+    </div>
+  );
+}
+
+export default Layout;
